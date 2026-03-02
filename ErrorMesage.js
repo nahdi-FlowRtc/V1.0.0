ca8dd4125cd31d6aeb720bec55c1c9d4993e061e
Ghaith <121437369+devtunis@users.noreply.github.com>
2026-03-02 10:31:20 +0100

    Refactor error messages and URLs in ErrorMesage.js
    Updated error messages and URLs for better clarity and accuracy.

--- ErrorMesage.js
+++ ErrorMesage.js
@@ -1,21 +1,16 @@
-// CameraErrors.js
-export const CAMERA_ERROR_MSG = "CameraError: Camera not initialized — See the doc mabe you find the solution";
-export const CAMERA_ERROR_URL = "https://docwebrtc/failedintalizecamera";
 
-
-export const CAMERA_ERROR_Permission = "CameraError : Camera user DenidPermision --see the doc mabe you find the solution"
-export const CAMERA_ERROR_Permission_URL = "https://docwebrtc/perimsionerror";
-
-
-
-
-export const ANSWER_ERROR = "Unable to get answer . Please check if you pass offer.";
-export const ANSWER_ERROR_URL = "https://docwebrtc/issue3/passTheOffetTotheAnswer";
-
-
-
-export const ESTABLISH_ERORR = "Unable to setRemoteDescription,Please Check the doc ."
-export const ESTABLISH_ERROR_URL = "http://docwebrtc/issue4/setremoteConnection"
-
-export const PASS_VIDEO_REF_ERROR = "Uable to pass videoRef ,Please Check the doc"
-export const PASS_VIDEO_REF_ERROR_URL = "http://docwebrtc/issue5/passRefVideo"
+//Error.js
+export const CAMERA_ERROR_MSG ="CameraError: Camera is not initialized. Please make sure to initialize the camera before using it.";
+export const CAMERA_ERROR_URL ="https://docwebrtc/failedinitializecamera";
+export const CAMERA_ERROR_Permission ="CameraError: Camera permission denied. Please allow camera access in your browser settings.";
+export const CAMERA_ERROR_Permission_URL ="https://docwebrtc/permissionerror";
+export const ANSWER_ERROR ="WebRTCError: Unable to create answer. Please ensure that a valid offer is provided.";
+export const ANSWER_ERROR_URL ="https://docwebrtc/issue3/passTheOfferToTheAnswer";
+export const ESTABLISH_ERORR ="WebRTCError: Unable to set remote description. Please check the documentation.";
+export const ESTABLISH_ERROR_URL ="https://docwebrtc/issue4/setRemoteConnection";
+export const PASS_VIDEO_REF_ERROR ="VideoError: Unable to use videoRef. Please provide a valid video reference.";
+export const PASS_VIDEO_REF_ERROR_URL ="https://docwebrtc/issue5/passRefVideo";
+export const Remote_ICE_ERROR ="WebRTCError: Remote description is not set. Cannot add ICE candidate.";
+export const Remote_ICE_ERROR_URL ="https://docwebrtc/issue6/remote_ice_error";
+export const JSON__ERROR      ="JsonError: Unexpected end of JSON input or mabe you donht pass valid object."
+export const JSON__ERROR__URL ="https://docwebrtc/issue7/JSON_ERROR";
